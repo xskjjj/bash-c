294ce68d65ccf7ed59d9ef218365c764e8c325a1
xskjjj <99611195+xskjjj@users.noreply.github.com>
2024-12-11 15:00:10 -0500

    Add files via upload

---
+++ coderback.bash
@@ -0,0 +1,56 @@
+#!/bin/bash
+#Name: Jingwen Xiao
+#Dapartment: Computer Science
+#Email: jingwen.xiao@mail.mcgill.ca
+
+#check if there is 2 parameter
+if [[ $# -ne 2 ]]
+then
+	echo "Error: Expected two inpt parameters."
+	echo "Usage: ./coderback.bash <backupdirectory><fileordirtobackup>"
+	exit 1
+fi
+
+#check if the first input exsit as a directory
+if [[ ! -d $1 ]]
+then
+	echo "Error: The directory '$1' does not exist."
+	exit 2
+fi
+
+#check if the second input exist as a directory or a file
+if [ -d $2 ] || [ -f $2 ] 
+then
+	:
+else
+	echo "Error: the directory or file '$2' does not exist."
+	exit 2
+fi
+
+#check if the both input are the same directory
+if [[ $1 -ef $2 ]]
+then
+	echo "Error: same directory for both input."
+	exit 2
+fi
+
+#backup
+
+if [[ ! -f $1/$(basename $2).$(date '+%Y%m%d').tar ]]
+then
+	tar -cf $(basename $2).$(date '+%Y%m%d').tar -P $2
+	mv $(basename $2).$(date '+%Y%m%d').tar $1
+else
+	echo "Backup file '"$2.$(date '+%Y%m%d').tar"' already exists. Overwrite? (y/n)"
+	read -p "Backup file '"$2.$(date '+%Y%m%d').tar"' already exists. Overwrite? (y/n)" CHOICE
+	if [ "$CHOICE" = "y" ]
+	then
+		tar -cf $(basename $2).$(date '+%Y%m%d').tar -P $2
+		mv -f  $(basename $2).$(date '+%Y%m%d').tar $1
+		exit 0
+	else
+		exit 3
+	fi
+
+fi
+
---
+++ deltad.bash
@@ -0,0 +1,73 @@
+#!/bin/bash
+#Name: Jingwen Xiao
+#Department: Computer Science
+#Email: jingwen.xiao@mail.mcgill.ca
+#Id: 260885501
+
+#check if there are 2 parameters
+if [[ $# -ne 2 ]]
+then
+	echo "Expected two input parameters."
+	echo "Usage: ./deltad.bash <originaldirectory> <comparisondirectory>"
+	exit 1
+fi
+
+#verify both input are directories and they are different directories
+if [[ ! -d $1 ]]
+then
+        echo "Input parameter #1 '$1' is not a directory."
+        echo "Usage: ./deltad.bash <originaldirectory> <comparisondirectory>"
+        exit 2
+elif [[ ! -d $2 ]]
+then
+        echo "Input parameter #2 '$2' is not a directory."
+        echo "Usage: ./deltad.bash <originaldirectory> <comparisondirectory>"
+        exit 2
+elif [[ $1 -ef $2 ]]
+then
+        echo "Error: Same directory for both input."
+        exit 2
+fi
+
+#check if both directories are empty
+DIR=$1
+DIR2=$2
+if [ ! "$(ls -A $DIR)" ] && [ ! "$(ls -A $DIR2)" ]
+then
+	exit 0
+fi
+
+#iterate the directory, to find the difference
+#iterate the directory, to find the difference
+RETURNO=0
+for FILE in $DIR/*.*
+do
+        if [[ ! -f $DIR2/"$(basename $FILE)" ]]
+        then
+                echo "$DIR2/"$(basename $FILE)" is missing"
+                ((RETURNO+=1))
+        else
+                diff -q $FILE $DIR2/"$(basename $FILE)" > /dev/null
+                if [ $? -eq 1 ]
+                then
+                        echo "$DIR2/"$(basename $FILE)" differs"
+                        ((RETURN+=1))
+                fi
+        fi
+done
+
+for FILE1 in $DIR2/*.*
+do
+        if [[ ! -f $DIR/"$(basename $FILE1)" ]]
+        then
+                echo "$DIR/"$(basename $FILE1)" is missing"
+                ((RETURNO+=1))
+        fi
+done
+
+if [[ $RETURNO -ne 0 ]]
+then
+        exit 3
+else
+        exit 0
+fi
